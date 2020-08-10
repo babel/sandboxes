bec6f74fcb38ccf3663728c9dc4d86de1b3ee47d
Garrett Luu <garrettluu131@gmail.com>
2020-08-10 12:35:24 -0700

    Added placeholder for null forks array

--- src/components/Forks.js
+++ src/components/Forks.js
@@ -4,8 +4,10 @@ import {Segment} from 'semantic-ui-react';
 
 export function Forks({id}) {
   function displayForks() {
-    // REPLState.GetBlob(id).forks;
-    return REPLState.GetBlob(id).forks?.map((fork) => (
+    // Check if there are forks to display, and use a placeholder if not
+    const forks = REPLState.GetBlob(id).forks ?? ['No forks to show'];
+    //Display each fork
+    return forks.map((fork) => (
       <Segment>{fork}</Segment>
     ));
   }
